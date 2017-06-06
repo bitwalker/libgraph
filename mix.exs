ad2d1137a619f54e644d361d088df3d1e1103f18
Paul Schoenfelder <paulschoenfelder@gmail.com>
2017-06-06 14:02:57 -0500

    Supports Elixir v1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Graph.Mixfile do
   def project do
     [app: :libgraph,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
