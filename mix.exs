7d65543363e26c0df90565c4d950f06ee52e4794
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-08-25 16:09:56 -0400

    Tweak deps and add bench aliases

--- mix.exs
+++ mix.exs
@@ -2,16 +2,29 @@ defmodule Graph.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :libgraph,
-     version: "0.12.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     elixirc_paths: elixirc_paths(Mix.env),
-     description: "A high-performance graph datastructure library for Elixir projects",
-     package: package(),
-     test_coverage: [tool: ExCoveralls]]
+    [
+      app: :libgraph,
+      version: "0.12.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env),
+      description: "A high-performance graph datastructure library for Elixir projects",
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      aliases: aliases(),
+      preferred_cli_env: [
+        "eqc.install": :test,
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        docs: :docs,
+        bench: :bench
+      ]
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -35,10 +48,21 @@ defmodule Graph.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:benchee, "~> 0.8", only: :dev},
-     {:eqc_ex, "~> 1.4", only: [:dev, :test]},
-     {:excoveralls, "~> 0.7", only: [:dev, :test]},
+    [{:benchee, "~> 0.8", only: [:bench]},
+     {:eqc_ex, "~> 1.4", only: [:test]},
+     {:excoveralls, "~> 0.7", only: [:test]},
      {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp aliases do
+    [
+      bench: ["bench.cliques", "bench.create", "bench.k_core", "bench.shortest_path", "bench.topsort"],
+      "bench.cliques": ["run bench/cliques.exs"],
+      "bench.create": ["run bench/create.exs"],
+      "bench.k_core": ["run bench/k_core.exs"],
+      "bench.shortest_path": ["run bench/shortest_path.exs"],
+      "bench.topsort": ["run bench/topsort.exs"]
+    ]
   end
 
   defp package do
