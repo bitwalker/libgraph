db395a758e2f55b28a69c88fc243b629c6494de5
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-08-25 16:10:07 -0400

    Version 0.13.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Graph.Mixfile do
   def project do
     [
       app: :libgraph,
-      version: "0.12.0",
+      version: "0.13.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
