1afe6b81c8579941a4e8b392d8f6df8157e309b1
Michael Bruderer <ojos@gmx.ch>
2018-12-05 17:42:53 +0100

    Add conversion from dot to png
    Add the function to_png, which compiles the dot file into a png file.

--- test/serializer_test.exs
+++ test/serializer_test.exs
@@ -18,6 +18,29 @@ defmodule Graph.SerializerTests do
     """} = Graph.to_dot(g)
   end
 
+
+  test "to_png/1" do
+    test_file = "test"
+    g = kitchen_sink_graph()
+
+    assert {:ok, ""} = Graph.to_png(g, test_file)
+    assert """
+    strict digraph {
+        "start"
+        "{:complex, :label}"
+        "c"
+        "finish"
+        "start" -> "{:complex, :label}" [weight=3]
+        "{:complex, :label}" -> "c" [label=5; weight=1]
+        "{:complex, :label}" -> "finish" [label=1.0; weight=3]
+        "c" -> "finish" [weight=1]
+    }
+    """ = File.read! "#{test_file}.dot"
+    assert File.exists? "#{test_file}.png"
+    File.rm "#{test_file}.dot"
+    File.rm "#{test_file}.png"
+  end
+
   test "to_edgelist" do
     g = kitchen_sink_graph()
 
