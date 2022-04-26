d04ae81626aa7ef8e340fd6b9316829178e714fb
Alex Kwiatkowski <alex+git@fremantle.io>
2022-04-25 18:42:10 -0700

    fix opts parameter order for benchmarks

--- bench/cliques.exs
+++ bench/cliques.exs
@@ -2,16 +2,20 @@ Code.require_file(Path.join([__DIR__, "..", "test", "support", "parser.ex"]))
 
 alias Graph.Test.Fixtures.Parser
 
-opts = %{
+opts = [
   time: 10,
   inputs: %{
     "Enron emails" => Path.join([__DIR__, "..", "test", "fixtures", "email-Enron.txt"]),
   }
-}
+]
+
 # This currently takes ages, only uncomment when you really want to run it
-Benchee.run(opts, %{
-      #"libgraph" => fn path ->
-              #g = Parser.parse(path)
-              #_ = Graph.cliques(g)
-            #end
-})
+Benchee.run(
+  # %{
+  #   "libgraph" => fn path ->
+  #     g = Parser.parse(path)
+  #     _ = Graph.cliques(g)
+  #   end
+  # },
+  opts
+)
--- bench/shortest_path.exs
+++ bench/shortest_path.exs
@@ -8,23 +8,28 @@ dg = Generators.libgraph_to_digraph(g)
 g2 = Generators.biased_dag(1_000)
 dg2 = Generators.libgraph_to_digraph(g2)
 
-opts = %{time: 15,
-         warmup: 5,
-         inputs: %{"unbiased" => {g, dg}, "biased" => {g2, dg2}}}
+opts = [
+  time: 15,
+  warmup: 5,
+  inputs: %{"unbiased" => {g, dg}, "biased" => {g2, dg2}}
+]
 
-Benchee.run(opts, %{
-      "digraph (get_short_path)" => fn {_, dg} ->
-        length(:digraph.get_short_path(dg, 1, 1_000)) != 0
-      end,
-      "libgraph (dijkstra)" => fn {g, _} ->
-        length(Graph.dijkstra(g, 1, 1_000)) != 0
-      end,
-      "libgraph (a_star)" => fn {g, _} ->
-        length(Graph.a_star(g, 1, 1_000, fn v ->
-              case Graph.vertex_label(g, v) do
-                nil -> 1_000
-                hint -> hint
-              end
-            end)) != 0
-      end,
-})
+Benchee.run(
+  %{
+    "digraph (get_short_path)" => fn {_, dg} ->
+      length(:digraph.get_short_path(dg, 1, 1_000)) != 0
+    end,
+    "libgraph (dijkstra)" => fn {g, _} ->
+      length(Graph.dijkstra(g, 1, 1_000)) != 0
+    end,
+    "libgraph (a_star)" => fn {g, _} ->
+      length(Graph.a_star(g, 1, 1_000, fn v ->
+        case Graph.vertex_label(g, v) do
+          nil -> 1_000
+          hint -> hint
+        end
+      end)) != 0
+    end,
+  },
+  opts
+)
